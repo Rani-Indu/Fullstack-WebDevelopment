e3f0738c73cb50ba259647ba3666bf2e3cebff74
Indu Rani <raniindu22@gmail.com>
2024-01-25 15:16:49 +0530

    add code

--- Apna clg.html/chap11 - class and object/class.js
+++ Apna clg.html/chap11 - class and object/class.js
@@ -1,43 +1,27 @@
 
 //   Inheritance
-
-// class Parent {
-// 	hello() {
-// 		console.log("hello");
-// 	}
-// }
-
-// class child extends Parent {}
-
-// let obj = new child()
+// super keyword
 
 class Person{
 	constructor(){
+		console.log("enter parent constructor");
 		this.species = "Homo Sapiens"
 	}
 	eat() {
 		console.log("eat");
 	}
-	sleep() {
-		console.log("sleep");
-	}
-	work() {
-		console.log("do nothing");
-	}
 }
 
 class Engineer extends Person {
+	constructor(branch){
+		console.log("enter child constructor");
+		super(); // to invoke / call parent class constructor
+		this.branch = branch;
+		console.log("exit child constructor");
+	}
 	work() {
 		console.log("solve problems, build something");
 	}
 }
-
-
-let induobj = new Engineer();                                
-// class Doctor extends Person {
-// 	work() {
-// 		console.log("treats patients");
-// 	}
-// }
-
-// let induobj = new Doctor();                                
+                          
+let engobj = new Engineer("mechanical engg");                          
