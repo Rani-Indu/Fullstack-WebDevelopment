caf577f92fe455eca4a6ca143f60341adeea760c
Indu Rani <raniindu22@gmail.com>
2024-01-25 14:41:54 +0530

    add code

--- Apna clg.html/chap11 - class and object/class.js
+++ Apna clg.html/chap11 - class and object/class.js
@@ -1,12 +1,43 @@
 
 //   Inheritance
 
-class Parent {
-	hello() {
-		console.log("hello");
+// class Parent {
+// 	hello() {
+// 		console.log("hello");
+// 	}
+// }
+
+// class child extends Parent {}
+
+// let obj = new child()
+
+class Person{
+	constructor(){
+		this.species = "Homo Sapiens"
+	}
+	eat() {
+		console.log("eat");
+	}
+	sleep() {
+		console.log("sleep");
+	}
+	work() {
+		console.log("do nothing");
 	}
 }
 
-class child extends Parent {}
+class Engineer extends Person {
+	work() {
+		console.log("solve problems, build something");
+	}
+}
 
-let obj = new child()
+
+let induobj = new Engineer();                                
+// class Doctor extends Person {
+// 	work() {
+// 		console.log("treats patients");
+// 	}
+// }
+
+// let induobj = new Doctor();                                
