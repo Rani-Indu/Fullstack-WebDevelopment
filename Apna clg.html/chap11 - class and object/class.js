b0ee41e9895d1d09b1e1584a6fcfecd00f5859f2
Indu Rani <raniindu22@gmail.com>
2024-01-26 18:33:40 +0530

    add codes

--- Apna clg.html/chap11 - class and object/class.js
+++ Apna clg.html/chap11 - class and object/class.js
@@ -1,19 +1,37 @@
+// function getData(dataId) {
+// 	console.log("data", dataId);
+// }
+// getData(213)
 
-// Try and Catch
-
-a = 5;
-b = 6;
-console.log("a = ", a);
-console.log("b = ",b);
-console.log("a+b = ", a+b);
-console.log("a-b = ", a-b);
-try {
-	console.log("c*b = ", c*b);   // error
-} catch (err) {
-	console.log(err);
+function getData(dataId, getNextData) {// after 2 sec
+	setTimeout(() => {
+		console.log("data", dataId);
+		if(getNextData) {
+			getNextData();
+		}	
+	}, 2000);
 }
-console.log("a/b = ", a/b);
-console.log("a+b = ", a+b);
-console.log("a-b = ", a-b);
-console.log("a*b = ", a*b);
-console.log("a/b = ", a/b);
+
+
+// getData(1, getData(2)) 
+// next data ke liye humne ushi function ko use kiya hai jo 1st ke liye use hua hai
+// but we'll get error if we right like above
+
+// correct method is 
+
+// getData(id, () => {
+// 	getData(id, () => {})
+// })
+
+getData(1, () => {
+	getData(2, () => {
+		getData(3, () => {
+			getData(4, () => {
+				getData(5, () => {
+					getData(6)
+				})
+			})
+		})
+	})
+});
+
