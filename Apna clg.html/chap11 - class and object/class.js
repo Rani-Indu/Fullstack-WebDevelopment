f3d85b2d9829568882d28ea943d021c260ae8d6e
Indu Rani <raniindu22@gmail.com>
2024-01-27 18:38:45 +0530

    add code

--- Apna clg.html/chap11 - class and object/class.js
+++ Apna clg.html/chap11 - class and object/class.js
@@ -3,14 +3,29 @@
 // }
 // getData(213)
 
-function getData(dataId, getNextData) {// after 2 sec
-	setTimeout(() => {
-		console.log("data", dataId);
-		if(getNextData) {
-			getNextData();
-		}	
-	}, 2000);
-}
+// function getData(dataId, getNextData) {// after 2 sec
+// 	setTimeout(() => {
+// 		console.log("data", dataId);
+// 		if(getNextData) {
+// 			getNextData();
+// 		}	
+// 	}, 2000);
+// }
+// function getData(dataId, getNextData) {// after 2 sec
+// 	setTimeout(() => {
+// 		console.log("data", dataId);
+// 		if(getNextData) {
+// 			getNextData();
+// 		}	
+// 	}, 2000);
+// }
+
+// format to write this function with setTimeout
+// function name(params) {
+// 	setTimeout(() => {	
+// 	}, timeout);
+// }
+
 
 
 // getData(1, getData(2)) 
@@ -19,7 +34,7 @@ function getData(dataId, getNextData) {// after 2 sec
 
 // correct method is 
 
-// getData(id, () => {
+// getData(id, () => {     // format to write this code
 // 	getData(id, () => {})
 // })
 
@@ -35,33 +50,78 @@ function getData(dataId, getNextData) {// after 2 sec
 // 	});
 // });
 
-getData(1, () => {
-	console.log("getting data 2.....");
-	getData(2, () => {
-		console.log("getting data 3.....");
-		getData(3, () => {
-			console.log("getting data 3.....");
-			getData(4, () => {console.log("getting data 4....."),getData(5)
-			});
-		});
-	});
-});
+// getData(1, () => {
+// 	console.log("getting data 2.....");
+// 	getData(2, () => {
+// 		console.log("getting data 3.....");
+// 		getData(3, () => {
+	// 			console.log("getting data 3.....");
+	// 			getData(4, () => {console.log("getting data 4....."),getData(5)
+	// 			});
+// 		});
+// 	});
+// });
 
 
 // Promises
 
-// let promise1 = new Promise((resolve, reject) => {
-// 	console.log("I am a promise");
-// });
+// let promise1 = new Promise ((resolved, reject) => {console.log("order placed");})
 
-// let promise1 = new Promise((resolve, reject) => {
-// 	console.log("I am a promise");
-// 	resolve("product delivered")
-// });
+// let promise2 = new Promise ((resolved, reject) => {console.log("order placed");
+// resolved("order delivered")})
 
-// let promise1 = new Promise((resolve, reject) => {
-// 	console.log("I am a promise");
-// 	reject("product canceled, refund initiated")
-// });
+// let promise3 = new Promise ((resolved, reject) => {console.log("order placed");
+// reject("order canceled")})
+
+//                Resolve
+// function getData(dataId, getNextData) {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			console.log("data", dataId);
+// 			resolve("success");
+// 			if(getNextData) {
+// 				getNextData();
+// 			}	
+// 		}, 5000);
+// 	})	
+// }
+
+// //                                Reject
+// function getData(dataId, getNextData) {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			console.log("data", dataId);
+// 			reject("error");
+// 			if(getNextData) {
+// 				getNextData();
+// 			}	
+// 		}, 5000);
+// 	})	
+// }
+
+// function name(params) {}
+	
+const getPromise = () => {
+	return new Promise((resolve, reject) => {
+		console.log("I am a promise");
+		resolve("success"); // res - is the message that we have passed here
+		reject("Network error");   // err - is the message that we have passed here
+	});
+};
+
+let promise = getPromise();
+promise.then((res) => {
+	console.log("promise fulfilled", res);
+});
+promise.catch((err) => {
+	console.log("rejected", err);
+});
+
+
+
+
+
+// let promise = getPromise
+
 
 
