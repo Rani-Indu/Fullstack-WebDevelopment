fdc7e035ef65dfa31373c32504e85d87b6695046
Indu Rani <raniindu22@gmail.com>
2024-02-10 16:26:10 +0530

    add codes

---
+++ Projects Using API/Weather App/app.js
@@ -0,0 +1,5 @@
+const inputbox = document.querySelector(".input-box");
+const searchbtn = document.querySelector(".search-btn");
+const weatherimg = document.querySelector(".weather-img");
+const windspeed = document.querySelector(".wind-speed");
+const humidity= document.getElementsByClassName("humidity");
