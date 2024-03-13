765abd77711945f28d035979d0fb6877b18b5231
Indu Rani <raniindu22@gmail.com>
2024-03-13 16:33:29 +0530

    add codes

---
+++ Day20 javascript/script1.js
@@ -0,0 +1,26 @@
+// Datatypes
+
+// Primitive Data Type
+
+// number - 1, 2, 3, 4, 5, 6
+// string - "indu" or 'rani'
+// boolean - true , false
+// null- null doesn't mean 0, null means nothing ex- on weather app we want to display temp of various places and if app is unable to find temp then display null (which mans nothing) as 0 is also a temp.
+// undefined -
+// example -
+// let a;
+// console.log(a);
+// output is  undefined - we have declared the variable and we can assign name later
+
+// Non Primitive Data Type
+
+// Array
+// let names = ["Radhe Radhe", "Hare Krishn","indu", "rani", 1, 2, 3, true, false];
+
+// let highScore = 400;
+// console.log(highScore);
+
+let courseFee = 300;
+let GstTax = 36;
+let finalAmount = courseFee + GstTax;
+console.log("Total Course Fee is =", finalAmount );
