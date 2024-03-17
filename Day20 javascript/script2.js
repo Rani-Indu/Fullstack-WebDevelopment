3ce3c2243ff1cf5533993cf102cd8464d28cd065
Indu Rani <raniindu22@gmail.com>
2024-03-17 15:11:48 +0530

    DOM

--- Day20 javascript/script2.js
+++ Day20 javascript/script2.js
@@ -1,12 +1,10 @@
-
 //                                  DOM - part 1
 
-//                  id   
-document.getElementById("one")
+//                  id
+document.getElementById("one");
 // console.log(document.getElementById("one"));;
 // console.log(document.getElementById("one").innerText);;
 // console.log(document.getElementById("one").innerHTML);;
-
 
 //                   class
 document.getElementsByClassName("tech");
@@ -19,18 +17,14 @@ let a = document.getElementsByClassName("tech");
 // a[2].innerText = "The Vamps"
 // console.log(a[2].innerText);
 
-
-
 //                    tagName
 // document.getElementsByTagName("h1");
-let b = document.getElementsByTagName("h1");
+// let b = document.getElementsByTagName("h1");
 // console.log(b);
 
-
-// wrong code 
+// wrong code
 // console.log(b.innerText);
 // as this line attempts to access innerText directly on the collection itself. However, innerText is not a property of an HTML collection; it's a property of individual HTML elements. Therefore, this line will likely throw an error, such as "Cannot read property 'innerText' of undefined" or "b.innerText is not a function".
-
 
 //    right code
 // console.log(b[0].innerText);
@@ -38,26 +32,23 @@ let b = document.getElementsByTagName("h1");
 // b[0].style.backgroundColor = "aqua"
 // console.log(b[1].innerText);
 
-
 // console.log(b.innerText);  // output - undefined
 // b.innerText = "Maa Radha"
 // console.log(b.innerText);   // output is Maa Radha
 
-
-
-
 //                                 query selector
-a = document.querySelector("h1")
-console.log(a);
 
-b = document.querySelector(".first")
+b = document.querySelector(".first");
 
-//  method to change name of already existing class 
-b.className = "Hola"
+//  method to change name of already existing class
+b.className = "Hola";
 console.log(b);
 
 // method to give multiple class
 // b.classList = "hello hola hi"
 // console.log(b);
-c = document.querySelector("#second")
-console.log(c);
+
+// b.setAttribute("id", "js")
+b.setAttribute("title", "indu");
+// b.setAttribute("Attribute ka naam", "Attribute ki value")
+
