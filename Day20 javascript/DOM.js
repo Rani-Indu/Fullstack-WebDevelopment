7a5e7b8523a7f023df9a3f4c8ed92133ba2cfa66
Indu Rani <raniindu22@gmail.com>
2024-03-17 20:01:43 +0530

    DOM EventListeners

--- Day20 javascript/DOM.js
+++ Day20 javascript/DOM.js
@@ -1,14 +1,14 @@
 //                                  DOM - part 1
 
 //                  id
-document.getElementById("one");
+// document.getElementById("one");
 // console.log(document.getElementById("one"));;
 // console.log(document.getElementById("one").innerText);;
 // console.log(document.getElementById("one").innerHTML);;
 
 //                   class
-document.getElementsByClassName("tech");
-let a = document.getElementsByClassName("tech");
+// document.getElementsByClassName("tech");
+// let a = document.getElementsByClassName("tech");
 // console.log(a);
 // console.log(a[0]);
 // console.log(a[0].innerText);
@@ -38,11 +38,11 @@ let a = document.getElementsByClassName("tech");
 
 //                                 query selector
 
-b = document.querySelector(".first");
+// b = document.querySelector(".first");
 
 //  method to change name of already existing class
-b.className = "Hola";
-console.log(b);
+// b.className = "Hola";
+// console.log(b);
 
 // method to give multiple class
 // b.classList = "hello hola hi"
@@ -50,9 +50,50 @@ console.log(b);
 
 // b.setAttribute("id", "js")
 // upon hover on first on browser we can see indu
-b.setAttribute("title", "indu");
+// b.setAttribute("title", "indu");
 // b.setAttribute("Attribute ka naam", "Attribute ki value")
 
 
 
-//                                   DOM - part 2
+// //                                   DOM - part 2
+
+// // Append Child
+// let title = document.createElement("h1");
+// title.className = "pwskills";
+// title.style.fontSize = "25px";
+// title.textContent = "PW Skills, Namaste";
+// console.log(title);
+// document.body.appendChild(title);
+
+// // Remove Child
+// let ul = document.querySelector("ul");
+// console.log(ul);
+// let lists = document.querySelectorAll("li");
+// console.log(lists);
+// for (const list of lists) {
+// 	// ul.removeChild(list)	
+// }
+
+
+
+//                                DOM - part 3
+// applying eventListener in entire document
+// document.addEventListener("click", hello);
+document.addEventListener("ondblclick", hello);
+// document.addEventListener("mouseover", hello);
+function hello() {
+	document.getElementById("js").innerText = "Namaste Bacho";
+}
+
+function hey() {
+	document.getElementById("btn").innerText = "Namaste";
+	document.getElementById("btn").style.color = "red";
+	document.getElementById("btn").style.padding = "1rem";
+	document.getElementById("btn").style.fontSize = "2rem";
+	document.getElementById("btn").style.border = "3px solid aqua";
+	document.getElementById("btn").style.backgroundColor = "black";
+	
+	
+}
+
+
