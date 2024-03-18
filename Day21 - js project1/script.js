1346051660343848e84f5ea62bcb4710ff2f7686
Indu Rani <raniindu22@gmail.com>
2024-03-18 18:22:43 +0530

    add codes

--- Day21 - js project1/script.js
+++ Day21 - js project1/script.js
@@ -3,13 +3,15 @@ const button = document.querySelector("#btn");
 
 const randomColor = () => {
 	let val = "0123456789ABCDEF";
-	console.log(val.length);
 	let cons = "#";
 	for(let i=0; i < 6; i++){
-		cons = cons + val[Math.floor(Math.random() * val.length)];
-		console.log(cons);
-		
+		cons = cons + val[Math.floor(Math.random() * val.length)];	
 	}
+	console.log(cons);
+	return cons;
 };
 
-button.addEventListener("click", randomColor);
+button.addEventListener("click", () => {
+	document.body.style.backgroundColor = randomColor();
+	button.style.color = randomColor();
+});
