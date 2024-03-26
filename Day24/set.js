70f660d0149e4eae0383d21e86b837b024228718
Indu Rani <raniindu22@gmail.com>
2024-03-26 16:03:29 +0530

    set

--- Day24/set.js
+++ Day24/set.js
@@ -20,10 +20,33 @@ let newSet = new Set([...myArray])  // spread operator
 // newSet.add(2)
 // console.log(newSet);
 
-newSet.add(9)
-newSet.clear(9)
-console.log(newSet);
+// newSet.add(9)
+// newSet.clear()
+// clears entire set
+// console.log(newSet);
 
 // newSet.add(22)
 // console.log(newSet);
 // console.log(newSet.has(9));
+
+// set difference
+function setDifference(set1, set2){
+	// Convert arrays to sets
+	const setA = new Set(set1);
+    const setB = new Set(set2);
+	return new Set([...setA].filter(el => !setB.has(el)))
+}
+
+let difference = setDifference([1, 2, 3], [4, 5, 6])
+console.log(difference);
+console.log([...difference]);
+
+
+// function setDifference(set1, set2){
+// 	const setA = new Set(set1)
+// 	const setB = new Set(set2)
+// 	return new Set([...setA].filter(el => !setB.has(el)))
+// }
+
+// difference = setDifference([1, 2, 3, 4], [7, 8, 9])
+// console.log(difference);
