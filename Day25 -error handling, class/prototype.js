a7c85c6259cb159cd4ae2adc904730e16d2cd031
Indu Rani <raniindu22@gmail.com>
2024-03-30 15:47:57 +0530

    prototype

---
+++ Day25 -error handling, class/prototype.js
@@ -0,0 +1,23 @@
+let myHeroes = ["Hanuman","thor"]
+let dcHeroes = ["spiderman", "batman", "flash"]
+
+let heroPower = {
+	thor: "hammer",
+	spiderman: "web",
+
+	// object ke andar hum methods bhi declare kar sakte hai
+	getspidermanPower: function (){
+		console.log(`spidy power is ${this.spiderman
+		}`);
+	}
+}
+
+// hum object ke andar inject kar rahe hai so ye sab ko available hoga, strings ko , arrays ko, kyuki js me sab kuchhi object hai
+Object.prototype.indu = function(){
+	console.log(`indu is present in all objects`);
+}
+console.log(myHeroes.indu());
+console.log(heroPower.indu());
+
+// jaisa ki sab kuch hi object hai to sab ko access mil gaya but 
+// abhi sirf array ko access denahai
