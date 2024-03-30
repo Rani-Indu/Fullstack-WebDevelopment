bd9ce5acd8a6bf2a2845c5e9ea33f00be11408fc
Indu Rani <raniindu22@gmail.com>
2024-03-30 19:19:49 +0530

    prototype

--- Day25 -error handling, class/prototype.js
+++ Day25 -error handling, class/prototype.js
@@ -12,12 +12,18 @@ let heroPower = {
 	}
 }
 
+
+//                                        object
+
 // hum object ke andar inject kar rahe hai so ye sab ko available hoga, strings ko , arrays ko, kyuki js me sab kuchhi object hai
 Object.prototype.indu = function(){
 	console.log(`indu is present in all objects`);
 }
 // console.log(myHeroes.indu());
 // console.log(heroPower.indu());
+
+
+//                                  Array
 
 // jaisa ki sab kuch hi object hai to sab ko access mil gaya but 
 // abhi sirf array ko access denahai
@@ -29,3 +35,46 @@ Array.prototype.heyIndu = function(){
 // console.log(heroPower.indu());
 // console.log(myHeroes.heyIndu());
 // console.log(heroPower.heyIndu());  // error as we have injected in array and not in object
+
+
+//                     Inheritance
+
+const User = {
+	name: "top name",
+	email: "top@gmail.com"
+}
+
+const Teacher = {
+	makeVideos: true
+}
+
+const TeachingSupport = {
+	isAvailable: false
+}
+
+const TAAssistant = {
+	makeAssignment: "JS Assignment",
+	fulltime: true,
+	__proto__: Teacher 
+}
+
+//                 OR
+
+Teacher.__proto__ = User
+//                 OR
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+console.log(TAAssistant.isAvailable);
+
+
+//                      string
+String.prototype.truelength = function(){
+	console.log(`true length is ${this.trim().length}`);   // chaining
+	// yaha hai ek hi string hai isliye this use kiya hai
+	// object me bahot saari values hoti hai isliye this.valuename use karte hai
+}
+
+"indu        ".truelength()
+"  rani  ".truelength()
+// hum kaise pata karenge o/pmme jo length hai wo in dono me se kis string ki hai
+// iske liye hum use karte hai this keyword se - this ka matlab hai wo string jis pe hm o/p  caahte hai / jis pe hum kaam kar rahe hai
