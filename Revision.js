332e3edf4cc0277a5ed5c910d9f01185a25345f4
Indu Rani <raniindu22@gmail.com>
2024-04-02 20:41:06 +0530

    working of js

--- Revision.js
+++ Revision.js
@@ -1,15 +1,38 @@
-// Array
-let names = ["Radhe Radhe", "Hare Krishn","indu", "rani", 1, "Anurg", 3, true, false];
-console.log(names[names.length - 4]);
+// // Array
+// let names = ["Radhe Radhe", "Hare Krishn","indu", "rani", 1, "Anurg", 3, true, false];
+// console.log(names[names.length - 4]);
 
-//    manipulation when we know the index
-names[names.length - 4] = "Anurag"
-console.log(names);
+// //    manipulation when we know the index
+// names[names.length - 4] = "Anurag"
+// console.log(names);
 
-//    manipulation when we don't know the index
-for(let i = 0; i < names.length; i++ ){
-	if(names[i] === "Anurg"){
-		names[i] = "Anurag Sir"
-	}
+// //    manipulation when we don't know the index
+// for(let i = 0; i < names.length; i++ ){
+// 	if(names[i] === "Anurg"){
+// 		names[i] = "Anurag Sir"
+// 	}
+// }
+// console.log(names);
+
+
+
+
+
+//                       Execution of JavaScript - working of function
+
+// use of debugger
+var vari1 = 1;
+one();
+two();
+console.log(vari1);
+function one() {
+	var a = 10;
+	console.log(a);
 }
-console.log(names);
+
+function two (){
+	var b = 20;
+	console.log(b);
+}
+
+
