03ec16e307553b4882fb0d5755504921355a2081
Indu Rani <raniindu22@gmail.com>
2024-04-29 18:29:59 +0530

    path module

---
+++ Day30_path module_nodejs/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+console.log(path.sep);
+
+console.log(process.env.PATH);
+
+console.log(path.delimiter);
+
+const filePath1 = '/public_html/home/index.html';
+
+const currentFilePath =  __filename;
+// console.log(currentFilePath);
+
+const currentFilePath1 =  __dirname;
+// console.log(currentFilePath1);
+
