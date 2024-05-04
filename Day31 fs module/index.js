6bf8c7aa7a42fb68ea288befd0d3b540a347760f
Indu Rani <raniindu22@gmail.com>
2024-05-04 21:27:31 +0530

    close a file

--- Day31 fs module/index.js
+++ Day31 fs module/index.js
@@ -23,43 +23,67 @@ console.log("OTHER STUFF");
 
 //     Read = open + Read
 
-const buf = new Buffer(1024);
-fs.open("input.txt", "r+", function (err, fd) {
-  if (err) {
-    console.log("error in opening files: ", err);
-  }
-  console.log("File open successfully");
+// const buf = new Buffer(1024);
+// fs.open("input.txt", "r+", function (err, fd) {
+//   if (err) {
+//     console.log("error in opening files: ", err);
+//   }
+//   console.log("File open successfully");
 
-  //   fs.read(fd, buf, 0, buf.length, 0, function(er, data){
-  //   fs.read(fd, buf, 0, buf.length, 0, function(er, bytes){
-  fs.read(fd, buf, 0, buf.length, 9, function (er, bytes) {
-    if (er) {
-      console.log("error inreading file");
-    }
-    // console.log('Data:', data.toString());
-    console.log("Data:", bytes);
-    console.log("Data:", buf.slice(0, bytes).toString());
-  });
-});
+//   //   fs.read(fd, buf, 0, buf.length, 0, function(er, data){
+//   //   fs.read(fd, buf, 0, buf.length, 0, function(er, bytes){
+//   fs.read(fd, buf, 0, buf.length, 9, function (er, bytes) {
+//     if (er) {
+//       console.log("error inreading file");
+//     }
+//     // console.log('Data:', data.toString());
+//     console.log("Data:", bytes);
+//     console.log("Data:", buf.slice(0, bytes).toString());
+//   });
+// });
 
 
 //                       writing to file
 
 fs.writeFile("input.txt", "PW Skills", function (err) {
-  if (err) {
-    console.log("Error in writing file");
-  } else {
-    console.log("Success in writing file");
-  }
+    if (err) {
+        console.log("Error in writing file");
+    } else {
+        console.log("Success in writing file");
+    }
 });
 
 
 //                        append to file
 fs.appendFile('input.txt', '__ Indu Rani','utf8', function(err){
-// fs.appendFile('input.text', '__ Indu Rani','utf8', function(err){
-    if (err) {
-        console.log("Error in appending file");
-    } else {
-        console.log("Success in appending file");
-    }
-})
+    // fs.appendFile('input.text', '__ Indu Rani','utf8', function(err){
+        if (err) {
+            console.log("Error in appending file");
+        } else {
+            console.log("Success in appending file");
+        }
+    })
+    
+//                            close a file
+    const buf = new Buffer(1024);
+    fs.open("input.txt", "r+", function (err, fd) {
+      if (err) {
+        console.log("error in opening files: ", err);
+      }
+      console.log("File open successfully");
+    
+      fs.read(fd, buf, 0, buf.length, 9, function (er, bytes) {
+        if (er) {
+          console.log("error inreading file");
+        }
+        console.log("Data:", bytes);
+        console.log("Data:", buf.slice(0, bytes).toString());
+        fs.close(fd, function(err){
+            if (err){
+                console.log("Error in closing file");
+            } else {
+                console.log("success in closing file");
+            }
+        })
+      });
+    });
