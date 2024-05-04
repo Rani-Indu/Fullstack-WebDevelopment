bd965045a20e56d6452e3e28fc293d5e546dbc1c
Indu Rani <raniindu22@gmail.com>
2024-05-04 20:55:00 +0530

    append to a file

--- Day31 fs module/index.js
+++ Day31 fs module/index.js
@@ -16,7 +16,7 @@ console.log("READ START");
 
 // synchronous way to read file
 // var data = fs.readFileSync("input.text");
-var data = fs.readFileSync('input.txt')
+var data = fs.readFileSync("input.txt");
 console.log("Data:", data.toString());
 console.log("READ END");
 console.log("OTHER STUFF");
@@ -28,16 +28,38 @@ fs.open("input.txt", "r+", function (err, fd) {
   if (err) {
     console.log("error in opening files: ", err);
   }
-  console.log('File open successfully');
-  
-//   fs.read(fd, buf, 0, buf.length, 0, function(er, data){
-//   fs.read(fd, buf, 0, buf.length, 0, function(er, bytes){
-  fs.read(fd, buf, 0, buf.length, 9, function(er, bytes){
-      if(er){
-          console.log("error inreading file")
-        }
-        // console.log('Data:', data.toString());
-        console.log('Data:', bytes);
-        console.log('Data:', buf.slice(0, bytes).toString());
-    })
+  console.log("File open successfully");
+
+  //   fs.read(fd, buf, 0, buf.length, 0, function(er, data){
+  //   fs.read(fd, buf, 0, buf.length, 0, function(er, bytes){
+  fs.read(fd, buf, 0, buf.length, 9, function (er, bytes) {
+    if (er) {
+      console.log("error inreading file");
+    }
+    // console.log('Data:', data.toString());
+    console.log("Data:", bytes);
+    console.log("Data:", buf.slice(0, bytes).toString());
+  });
 });
+
+
+//                       writing to file
+
+fs.writeFile("input.txt", "PW Skills", function (err) {
+  if (err) {
+    console.log("Error in writing file");
+  } else {
+    console.log("Success in writing file");
+  }
+});
+
+
+//                        append to file
+fs.appendFile('input.txt', '__ Indu Rani','utf8', function(err){
+// fs.appendFile('input.text', '__ Indu Rani','utf8', function(err){
+    if (err) {
+        console.log("Error in appending file");
+    } else {
+        console.log("Success in appending file");
+    }
+})
