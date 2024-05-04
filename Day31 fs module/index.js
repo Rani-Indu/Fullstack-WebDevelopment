87b5dd39cee9f2cad47e6bcec84680d7d70c0911
Indu Rani <raniindu22@gmail.com>
2024-05-04 19:01:30 +0530

    sync , async method to read files

--- Day31 fs module/index.js
+++ Day31 fs module/index.js
@@ -2,16 +2,24 @@ const fs = require('fs');
 
 
 console.log('READ START');
-// Asynchronous way to read file
-fs.readFile('input.txt',function(err, data) {
-    if (err) {
-        console.log('Error:', err);
-        return err;
-    }
-    console.log('Data:', data);
-    console.log('Data:', data.toString());
-    console.log('READ END');
-    return data;
-})
+//                                                                                                                                                                                                                                                     Asynchronous way to read file
+// fs.readFile('input.txt',function(err, data) {
+//     if (err) {
+//         console.log('Error:', err);
+//         return err;
+//     }
+//     console.log('Data:', data);
+//     console.log('Data:', data.toString());
+//     console.log('READ END');
+//     return data;
+// })
+// console.log('OTHER STUFF');
+
+
+// synchronous way to read file
+var data = fs.readFileSync('input.text')
+// var data = fs.readFileSync('input.txt')
+console.log('Data:', data.toString());
+console.log('READ END');
 console.log('OTHER STUFF');
 
