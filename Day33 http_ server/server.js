6b332db52821635762d614256a3bbb0b12fe2051
Indu Rani <raniindu22@gmail.com>
2024-05-06 21:02:50 +0530

    http module

---
+++ Day33 http_ server/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.write('<h1>Hello, Nodejs !</h1>'); 
+    } else if(req.url == '/about'){
+        res.write()
+    }
+    res.end('<h1>About page !</h1>');
+});
+
+server.listen(5001);
+console.log('the HTTP server is running on port 5001');
