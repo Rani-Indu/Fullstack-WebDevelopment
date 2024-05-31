e5f40466094a2f6913cecd8a748a416f4c95a737
Indu Rani <raniindu22@gmail.com>
2024-05-31 21:20:56 +0530

    revision - creating server using node.js

--- Day33 http_ server/practice/server.js
+++ Day33 http_ server/practice/server.js
@@ -11,19 +11,79 @@
 // console.log('server is running at port 2000');
 
 
+// const http = require('http');
+// const PORT = 2020;
+// HOSTNAME = 'localhost';
+
+// const Server = http.createServer((req, res) => {
+
+//     // res.statusCode = 200;
+//     // res.setHeader('content-type', 'text/plain');
+//     // res.end('node server created by me');
+
+//     res.statusCode = 500;
+//     res.setHeader('content-type', 'application/json');
+//     res.end(JSON.stringify({error: 'Server Error!'}));
+// });
+
+// Server.listen(PORT, () => {
+//     console.log(`Server is running at ${HOSTNAME}: ${PORT}`);
+// });
+
+
 const http = require('http');
-const PORT = 2020;
+const PORT = 3020;
 HOSTNAME = 'localhost';
 
 const Server = http.createServer((req, res) => {
-
-    // res.statusCode = 200;
-    // res.setHeader('content-type', 'text/plain');
-    // res.end('node server created by me');
-
-    res.statusCode = 500;
-    res.setHeader('content-type', 'application/json');
-    res.end(JSON.stringify({error: 'Server Error!'}));
+    if(req.url = '/'){
+        res.statusCode == 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello world');
+    }
+    else if(req.url == '/about'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('about Page!');
+    }
+    else if(req.url == '/contact'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('contact Page!');
+    }
+    // else if(req.url == '/product'){
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/plain');
+    //     res.end(JSON.stringify({productName: "Product 1"}));
+    // }
+    
+    else if(req.url == '/product'){
+        const options = {
+            hostname: 'fakestoreapi.com',
+            path: '/products/1',
+            method: 'GET'
+        }
+        
+        const apiReq = http.request(options, (apiRes) => {
+            apiRes.on("data", (data) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(data.toString()));
+                
+            })
+        });
+        
+        apiReq.on('error', () => {
+            console.log(e);
+        })
+        
+        apiReq.end();
+    }
+    else {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('server error!');
+    }
 });
 
 Server.listen(PORT, () => {
