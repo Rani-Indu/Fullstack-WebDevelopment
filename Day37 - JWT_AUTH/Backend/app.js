00dc7a6e3ea3eda63f09f8ceaab48671af338a20
Indu Rani <raniindu22@gmail.com>
2024-06-05 21:23:43 +0530

    Auth with Express

---
+++ Day37 - JWT_AUTH/Backend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+    res.status(200).json({data: 'JWTauth server'})
+})
+
+// since this is a module so we can export it so that we can import in any other place where required
+
+module.exports = app;
