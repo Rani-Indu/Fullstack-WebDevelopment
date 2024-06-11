82e70f0e69ca13d2d37da9f5f15529524ded6fad
Indu Rani <raniindu22@gmail.com>
2024-06-11 21:27:49 +0530

    static req

--- Day37 - JWT_AUTH/Backend/app.js
+++ Day37 - JWT_AUTH/Backend/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const authRouter = require('./router/authRoutes');
 const app = express();
-const  authRouter = require('./router/authRoutes') 
+// const  authRouter = require('./router/authRoutes') 
+app.use(express.json());
 
 
-app.use('', authRouter)  // authRouter kaha se milega
+app.use('/api/auth/', authRouter)  // authRouter kaha se milega
 
 app.use('/', (req, res) => {
     res.status(200).json({data: 'JWTauth server - updated'})
 })
 
-// since this is a module so we can export it so that we can import in any other place where required
+// since this is a module so we can export it so that we can import it at other place where required
 
 module.exports = app;
 
--- Day37 - JWT_AUTH/Backend/controller/authController.js
+++ Day37 - JWT_AUTH/Backend/controller/authController.js
@@ -1,4 +1,10 @@
 const signup = (req, res, next) => {
+    const {name, email, password, confirmPasswoed } = req.body;
+    console.log(name, email, password, confirmPasswoed );
+    return res.status(200).json({
+        success: true,
+        data: {}
+    })
 
 }
 
