0493f47e9f68e0bf08dff56fe72c42902529e913
Indu Rani <raniindu22@gmail.com>
2024-07-11 21:05:44 +0530

    add codes

--- Backend Revision/source/index.js
+++ Backend Revision/source/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+import app from './app.js';
+const port = 3000
+
+// database connection
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+// Points to remember while making database connection
+// Database connection may fail
+// Database is always in another continent  
+
+// Database connections may fail means  - agar connection karte waqt error aa gaya to - so error handle karna hoga - using try catch
+// Another continent means it will take time to give o/p so async await / promises is used to handle the time gap for o/p - as below using async IIFE
+
+// app.get('route', callback)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/instagram', (req, res) => {
+  res.send('<h1>visited instagram</h1> ')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('<h1>visited twitter</h1> ')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
