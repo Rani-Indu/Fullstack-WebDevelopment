b6050562be8b7280960ac23b7f552f7cf0c15f18
Indu Rani <raniindu22@gmail.com>
2024-07-12 21:14:21 +0530

    backend revision

--- Backend Revision/source/index.js
+++ Backend Revision/source/index.js
@@ -12,6 +12,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/test');
 // Database connections may fail means  - agar connection karte waqt error aa gaya to - so error handle karna hoga - using try catch
 // Another continent means it will take time to give o/p so async await / promises is used to handle the time gap for o/p - as below using async IIFE
 
+// async await IIFE and try catch
+(async() => {
+	try {
+    // db connection
+    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    console.log('db connected successfully');
+		
+	} catch (error) {
+	console.error("error", err);
+    throw err	
+	}
+})()
 // app.get('route', callback)
 app.get('/', (req, res) => {
   res.send('Hello World!')
