ce4ba5e35c4a724916e0cd9874844fc3a31f157d
Indu Rani <raniindu22@gmail.com>
2024-08-13 21:25:10 +0530

    add codes

--- Backend project 1 Revision/controllers/userController.js
+++ Backend project 1 Revision/controllers/userController.js
@@ -9,6 +9,17 @@ exports.createUser = async(req, res) => {
 	// extract info
 	try {
 		const{name, email} = req.body
+		// if name and email doesn't exist
+		if(!name || !email){
+			throw new Error("name and email are required")
+		}
+		// if user already exist with that email
+
+        // finding user on the basis of email by creating an object
+		const userExists = await User.findone({email})
+		if(userExists){
+			throw new Error("user already exists")
+		}
 
 		const user = await User.create({
 			name,
@@ -18,16 +29,30 @@ exports.createUser = async(req, res) => {
 		res.status(201).json({
 			success: true,
 			message: "User created successfully",
-			user
+			user,
 		})
 
 	} catch (error) {
 		console.log(error);
 		res.status(400).json({
 			success: false,
-			message: "error.message",
+			message: error.message,
 			// user nahi hai isliye user nahi bhej sakte
 		})		
 	}
 }
 
+
+exports.getUser = async(req, res) => {
+	try{
+		// const users = await user.find({name}) // search on basis of name
+		// const users = await user.find({email}) // search on basis of email
+		const users = await user.find({})  // no condition is mentioned so all users are required
+		res.status(200).json({
+			success: true,
+			users
+		})
+	}
+} 
+
+// 1:22:00
