216c1374af0498d4674575e425efadbeaa93990d
Indu Rani <raniindu22@gmail.com>
2024-08-23 20:03:31 +0530

    app using express

--- Backend project 1 Revision/server.js
+++ Backend project 1 Revision/server.js
@@ -2,5 +2,5 @@ const app = require('./app.js')
 const port = process.env.port || 5000
 
 app.listen(port, () => {
-	console.log (`server listening on port http://localhost:${port}...`);
+	console.log (`server listening on port http://localhost:${port}`);
 });  
