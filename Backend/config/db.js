853daa1502f8e0f00594d23c64bb57fa3a48afbc
Indu Rani <raniindu22@gmail.com>
2024-08-28 20:54:05 +0530

    db connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,2 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const connectToDb = async() => {
+    mongoose.connect(process.env.mongo_url)
+    .then((conn)=>{
+        console.log(`connected to db : ${conn.connection.host}`);
+    })
+    .catch((err) => {
+        console.log(err.message);
+        process.exit(1)
+    })
+};
+
+
+
+
+module.exports = connectToDb
