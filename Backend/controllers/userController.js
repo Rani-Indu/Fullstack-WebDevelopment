c7601eb18a92a7933e434e2880eb614e334aa6f9
Indu Rani <raniindu22@gmail.com>
2024-09-04 20:54:46 +0530

    schema

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,3 +1,19 @@
+User = require("../models/userModel");
+
 exports.home = (req, res) => {
     res.send('Hello World!')
+  }
+
+  exports.createUser = (req, res) => {
+    //extract info
+    try {
+        const {name, email} = req.body
+        // const user = await User.create({
+        //   name,
+        //   email
+        // })
+        
+    } catch (error) {
+        
+    }
   }
