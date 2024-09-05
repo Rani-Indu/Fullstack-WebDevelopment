6ae8d6b912c177744b4f1c8991c1a0b9d8b30f27
Indu Rani <raniindu22@gmail.com>
2024-09-05 14:32:42 +0530

    data saved in db

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -4,14 +4,15 @@ exports.home = (req, res) => {
     res.send('Hello World!')
   }
 
-  exports.createUser = (req, res) => {
+  exports.createUser = async(req, res) => {
     //extract info
     try {
         const {name, email} = req.body
-        // const user = await User.create({
-        //   name,
-        //   email
-        // })
+        const user = await User.create({
+          name,
+          email
+
+        })
         
     } catch (error) {
         
