bfaad21efe5ccdf47ef6a58b21bcb36de9aa4837
Indu Rani <raniindu22@gmail.com>
2024-09-09 20:26:36 +0530

    auth with express

--- Day37 - JWT_AUTH/Backend/router/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { signup } = require('../controller/authController');
-const authRouter = express.Router(); // creating router instance
-
-// route('path', controller)
-authRouter.post('/signup', signup )
-
-module.exports = authRouter;
