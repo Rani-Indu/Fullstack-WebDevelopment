65575b1c3f9f401e55f72b7d75a0a338f5d4374a
Indu Rani <raniindu22@gmail.com>
2024-09-14 19:31:01 +0530

    userSchema

--- Day37 - JWT_AUTH/models/authModel.js
+++ Day37 - JWT_AUTH/models/authModel.js
@@ -5,13 +5,28 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'name is required'],
         trim: true,
+        minLength: [4, 'name must be at least characters'],
         maxLength: [50, 'name must be less than 50 character']
     },
     email: {
         type: String,
         required: [true, 'email is required'],
-        unique: true,
+        unique: [true, 'already registered'],
+        lowercase: true,
+    },
+    // hum dekh sakte hai ki kitna powerful hai mongoose, humne user me kuch aur data diya hai , hum ush data ko sanetize kar ke, better tarike se store karna cahate hai db me, automatically mongoose hume ye karne me help kar raha hai 
+    password: {
+        type: String,
+        select: false,
+    },
+    // password will expire after sometime
+    forgotPasswordToken: {
+        type: String
+    },
+    // expiry duration
+    forgotPasswordExpiryDate: {
+        type: Date
     }
-    });
+    }, {timestamps: true});
 
-    module.exports = mongoose.model("user", userSchema)
+    module.exports = mongoose.model("JWTauth", userSchema)
