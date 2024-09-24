b383dc293c72ae0b8daeae1ffca7e581f14deb56
Indu Rani <raniindu22@gmail.com>
2024-09-24 15:28:05 +0530

    logout

--- Day37 - JWT_AUTH/router/authRouter.js
+++ Day37 - JWT_AUTH/router/authRouter.js
@@ -13,8 +13,7 @@ const {signup, signin, getUser, logout} = require('../controller/authController'
 authRouter.post('/signup', signup);
 authRouter.post('/signin', signin);
 authRouter.get('/user', jwtAuth, getUser);
-authRouter.post('/logout',jwtAuth, logout); //logout karne ke liye user ka logged in hona zaroori hai , so jwtAuth use karenge , ensure karne ke liye ki user logged in ho
-// agar koi bhi logged in user ka route use karna hai to jwtAuth middleware zaroor use karenge, taki ensure kar sake ki ye operation logged in user ke liye kar rahe hai
+authRouter.post('/logout',jwtAuth, logout); 
 
 
 
