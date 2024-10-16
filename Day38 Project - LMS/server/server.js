de4c8832af264738700fdd9f6bd67fe5acd722d4
Indu Rani <raniindu22@gmail.com>
2024-10-16 21:06:39 +0530

    cloudinary configuration

--- Day38 Project - LMS/server/server.js
+++ Day38 Project - LMS/server/server.js
@@ -1,8 +1,16 @@
 import app from './app.js';
+import connectToDb from './config/dbConnection.js';
+import cloudinary from 'cloudinary';
+
 const port = process.env.PORT || 3000
 
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
-
-app.listen(port, () => {
-    console.log(`app is listening at http://localhost:${port}`)
+app.listen(port, async () => {
+  await connectToDb
+  console.log(`app is listening at http://localhost:${port}`)
   })
