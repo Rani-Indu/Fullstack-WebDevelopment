1e0b8becc7e40c47759ecff149791b306d3dbaac
Indu Rani <raniindu22@gmail.com>
2024-10-19 21:12:18 +0530

    axios

--- Day39_Backend_complete_concept/Backend/server.js
+++ Day39_Backend_complete_concept/Backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('server is ready')
-})
+// app.get('/', (req, res) => {
+//     res.send('server is ready')
+// })
 
 const port = process.env.PORT || 3000;
 
