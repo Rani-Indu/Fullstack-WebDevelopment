664889952f6a940a12957aa3ac81c2e7b770ab01
Indu Rani <raniindu22@gmail.com>
2024-10-27 16:41:39 +0530

    utils

---
+++ Day40_ Backend_Project/utils/asyncHandler.js
@@ -0,0 +1,47 @@
+// utils containd methods/ code  which are repetitive
+
+// const asyncHandler = (err, req, res, next) => {}
+    
+    
+    
+// HOF/ HOM
+// Functions js ke andar parameters ke tarah pass ho sakte hai, aur hum caahe to function ki value ko return bhi kar sakte hai
+// asyncHandler - acts like HOF
+// HOF - wo functions jo function ko as a parameter bhi accept kar sakte hai ya phir usko return kar sakte hai 
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}}
+    // const asyncHandler = (func) => () => {} //remove curly braces , one and same thing
+    
+    
+    
+        
+        // try catch method
+        // const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+    //         await fn(req, res, next)
+    //     } catch (error) {
+        //         res.status(err.code || 500).json({
+        //             success: false,
+        //             message: err.message
+//         })
+
+//     }
+// } 
+
+
+// .then .catch method
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+
+
+
+
+
+export default asyncHandler
