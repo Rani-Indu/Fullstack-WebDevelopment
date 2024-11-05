0cef8252d7973fea7dd7887bd294ac9bbb12ccff
Indu Rani <raniindu22@gmail.com>
2024-11-05 14:53:13 +0530

    upload file On Cloudinary

--- Day40_ Backend_Project/utils/cloudinary.js
+++ Day40_ Backend_Project/utils/cloudinary.js
@@ -1,25 +1,33 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs";
+// fs - file system - https://nodejs.org/api/fs.html 
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_ssecret: process.env.CLOUDINARY_API_SECRET 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 
-// const uploadOnCloudinary = async () => {}
-// const uploadOnCloudinary = async () => {
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// }
+
 const uploadOnCloudinary = async (localFilePath) => {
     try {
+         
+        if (!localFilePath) return null
+        
+        cloudinary.uploader.upload(localFilePath, {
+            // ctrl + space to see available options 
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("file is uploaded successfully", response.url);
+        return response;
         
     } catch (error) {
-        
+        fs.unlinkSync(localFilePath) // removes the locally saved temporary file as the upload operation got failed
+        return null; 
     }
 }
+
+
+export {uploadOnCloudinary}
