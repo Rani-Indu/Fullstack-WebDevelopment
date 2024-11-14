366986116e6f3fc0e3e634b62117ceb88de8222f
Indu Rani <raniindu22@gmail.com>
2024-11-14 20:36:30 +0530

    ApiResponse

--- Day40_ Backend_Project/controllers/user.controller.js
+++ Day40_ Backend_Project/controllers/user.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/api_error.js";
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 
@@ -53,12 +54,20 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
     username: username.toLowerCase()
-    // watch History humko cahiye nahi kyuki isko by default 0 orempty rakhna cahenge jaise jaise user video dekhega , waha pe jo api hit hogi waha pe aa kar ish model me wapas aa kar add karte jayenge chize , 
-    // refreshToken by default hum cahate hai ki empty ho , 
-    //time stamp apne aap mil jayega isko  
-    //save karte time password ko encrypt kar diya jayega 
   }) 
-
+  // we'll check ki sahi me user bana hai ya nahi ki empty return ho gaya hai , 1. we can directly check 
+  // 2. User.findByid - User agar successfully create hua hai to mongodb jab data save karta hai to har ek entry ke saath _id naam se ek field add kar deta hai  
+  
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+    // agar user create ho gaya hai to find karne pe mil jayega 
+  )
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while registering the usera")
+  }
+  // next step 
+return res.status(201).json({})
+// json response bhenjege - json response jayega uska architecture define hai 
 });
 
 
