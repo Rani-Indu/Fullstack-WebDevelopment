7666536d0011bc5ba881e7da1c55663d8e959f58
Indu Rani <raniindu22@gmail.com>
2024-11-17 16:00:39 +0530

    login user

--- Day40_ Backend_Project/controllers/user.controller.js
+++ Day40_ Backend_Project/controllers/user.controller.js
@@ -1,15 +1,13 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/api_error.js";
-import { User } from "../models/user.model.js"
-import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
-
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, username, password } = req.body;
   // console.log(req.boy);
-  
+
   console.log("email: ", email);
 
   // checking if any field is empty
@@ -19,45 +17,42 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-
   // checking if user already exist i,e already registered
   const existedUser = await User.findOne({
-    $or: [{ username }, { email }]  
-  })
+    $or: [{ username }, { email }],
+  });
   // console.log(existedUser);
 
-  if (existedUser) {throw new ApiError(409, "User with email or username already exists")
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
   }
 
   // checking for images, avatar
 
-  
   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
   //on writing this code we are getting error - {"success":false,"message":"Cannot read properties of undefined (reading '0')"}
   // so let's do it using if else
-  
+
   let coverImageLocalPath;
   // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
   if (req.files?.coverImage?.length > 0) {
     coverImageLocalPath = req.files.coverImage[0].path;
   }
   // console.log(req.files);
-  
+
   const avatarLocalPath = req.files?.avatar?.[0]?.path;
- 
+
   if (!avatarLocalPath) {
     console.log("req.files:", req.files);
-    throw new ApiError(400, "avatar file is required")
+    throw new ApiError(400, "avatar file is required");
   }
-  
-  const avatar = await uploadOnCloudinary(avatarLocalPath)
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
-    throw new ApiError(400, "avatar file is required") 
+    throw new ApiError(400, "avatar file is required");
   }
-
-
 
   User.create({
     fullname,
@@ -65,21 +60,21 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImage: coverImage?.url || "",
     email,
     password,
-    username: username.toLowerCase()
-  }) 
-  
+    username: username.toLowerCase(),
+  });
+
   const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken" 
-  )
+    "-password -refreshToken"
+  );
   if (!createdUser) {
-    throw new ApiError(500, "something went wrong while registering the user")
+    throw new ApiError(500, "something went wrong while registering the user");
   }
-  // next step 
-return res.status(201).json(
-  new ApiResponse(200, createdUser, "user registered successfully"))
+  // next step
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user registered successfully"));
 });
 
+const loginUser = asyncHandler(async (req, res) => {});
 
-export { registerUser };
-
-
+export { registerUser, loginUser };
