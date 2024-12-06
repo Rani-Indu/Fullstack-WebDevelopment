504d4e02895ef032d10f640fb5faa4bdb001d7f5
Indu Rani <raniindu22@gmail.com>
2024-12-06 19:44:15 +0530

    refreshAccessToken endpoint created

--- Day40_ Backend_Project/routes/user.routes.js
+++ Day40_ Backend_Project/routes/user.routes.js
@@ -31,7 +31,8 @@ router.route("/login").post(loginUser)
  
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken) 
+
 
 
 
