c8d449e99e0b2466173efbe98e3d91a8de91a0a3
Indu Rani <raniindu22@gmail.com>
2024-12-07 16:15:24 +0530

    changing current password

--- Day40_ Backend_Project/controllers/user.controller.js
+++ Day40_ Backend_Project/controllers/user.controller.js
@@ -223,6 +223,15 @@ const refreshAccessToken = asyncHandler(async(req, res) => {
 })
 
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+  const {oldPassword, newPassword} = req.body 
+  
+  const user = await User.findById(req.user?.id)
+  
+
+
+})
+
 
 
 
