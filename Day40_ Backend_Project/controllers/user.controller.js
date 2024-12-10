07f885e9b4e6cc74e1bdba81a52fcc4d159c5cc7
Indu Rani <raniindu22@gmail.com>
2024-12-10 15:22:53 +0530

    corrections in code

--- Day40_ Backend_Project/controllers/user.controller.js
+++ Day40_ Backend_Project/controllers/user.controller.js
@@ -250,7 +250,8 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    // .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "user fetched successfully"))
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -260,7 +261,8 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
+    // await as db call 
     req.user?._id,
     {
       $set: {
