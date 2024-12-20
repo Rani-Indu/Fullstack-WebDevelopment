53f0e8da5404c774e76b045f85b14f96a526de55
Indu Rani <raniindu22@gmail.com>
2024-12-20 20:12:33 +0530

    signin, getuser

--- Day37 - JWT_AUTH/controller/authController.js
+++ Day37 - JWT_AUTH/controller/authController.js
@@ -48,7 +48,6 @@ const signup = async (req, res, next) => {
       });
     }
     return res.status(400).json({
-      // agar data store nahi hua to error send karenge
       success: false,
       message: e.message,
     });
@@ -56,7 +55,6 @@ const signup = async (req, res, next) => {
 };
 
 const signin = async (req, res) => {
-  // if in I/p there is no email or password
   const { email, password } = req.body;
   if (!email || !password) {
     res.status(400).json({
@@ -68,7 +66,6 @@ const signin = async (req, res) => {
   try {
     const user = await userModel.findOne({ email }).select("+password");
 
-    // if (!user || user.password !== password) {
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(400).json({
         success: false,
@@ -76,29 +73,19 @@ const signin = async (req, res) => {
       });
     }
 
-
-
-    
-    //  upto here - mera username password sahi tha
-    // ab ek token generate kar dena hai
-
-    const token = user.jwtToken(); // user ke through jwt token mil gaya
-    user.password = undefined; // ish token me ek chiz ensure karni hai ki mere pass jo password hai wo leak nahi hona cahiye so password ko null set kar do
-
-    // ab hume cookie banani hai , token to generate ho gaya
-    // cookie me rakhne ke liye kuch chize hoti hai
+    const token = user.jwtToken(); 
+    user.password = undefined;  
     const cookieOption = {
-      // cookie 24hr ke liye valid rahe , ms(millisecond) me generate karenge
-      maxAge: 24 * 60 * 60 * 1000, // 24hr
-      httpOnly: true, // for security purpose, client side se , js se hum ishe access nahi kar sakte
+      maxAge: 24 * 60 * 60 * 1000, 
+      httpOnly: true, 
     };
 
-    // ye configuration de diya ab ishke response me cookie set karni hai, naam hai cookie ka "token", cookie ki value ke liye 3 option hote hai token - jo mera actual token hai , and uske options cookieOption, option is optional, koi read na kar sake client side se isliye httpOnly diya hai 8
+    
 
     res.cookie("token", token, cookieOption);
     res.status(200).json({
       success: true,
-      data: user, // humne password ko pehle hi null kar diya hai taki hum wapas se client ko password na bhej de
+      data: user,
     });
   } catch (e) {
     res.status(400).json({
@@ -109,23 +96,10 @@ const signin = async (req, res) => {
 };
 
 
-const getUser = async(req, res) => {
-  // user information ke liye hume user id cahiye taki uske corresponding hum db me ja kar query kar sake 
+const getUser = async(req, res) => { 
   const userId = req.user.id; 
-  //  1. hum user se har baar user id to mange ge nahi, kyuki already loggedin hu to cookie me information available hai to har baar information mangne ka sense hi nahi hai   
-  //  2. ye kaise validate kare ki already logged in hai ya nahi , yani kya hamare pass valid token hai 
-
-  // hum aconside karte hai ki hum already logged in hai aur hamare pass valid token hai - to db me query kar ke kaise information layenge dekhte hai 
   try {
     const user = await userModel.findById(userId);
-    // humne db se user ki sari information request ki hai yani password bhi aayega , but hum nahi cahte ki password reveal ho , iske liye humne schema me pehle se 
-    // password: {
-    //   type: String,
-    //   select: false,  
-     // ishse agar koi password nahi mang raha hai to password nahi milega ,
-     // },
-    //  jaha hume password cahiye waha hum aise le sakte hai 
-    // const user = await userModel.findOne({ email }).select("+password");
     return res.status(200).json({
       success: true,
       data: user
@@ -139,16 +113,6 @@ const getUser = async(req, res) => {
   }
 }
 
-// userController me 
-// 1. user ki information kaun dega
-// 2. validate kaun karega user logged in hai ya nahi 
-// uske liye hum banayenge middleware
-// middleware kis liye kaam aayega- humne login karne pe jo token generate kiya tha, jwt token ke through jaha pe humne id and email rakhi thi in schema 
-// return JWT.sign(
-        // part 1 - data
-        // {id: this._id, email: this.email},
-        // ye informatio stored hai client side pe token me 
-        // kya hum ush token ko decrypt kar ke id nikal le aur request me daal le to hamara kaam ho jayega - yes, ho jayega
 
 const logout = (req, res) => {
   try {
