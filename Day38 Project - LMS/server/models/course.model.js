aba468e616e2b21f20e3802d2a23acda5e6e68dc
Indu Rani <raniindu22@gmail.com>
2025-01-27 15:30:34 +0530

    course schema created

---
+++ Day38 Project - LMS/server/models/course.model.js
@@ -0,0 +1,56 @@
+import { model, Schema } from "mongoose";
+
+const courseSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    maxLength: [8, "Title must be atleast 8 characters"],
+    minLength: [59, "Title should be less than 60 characters"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+    maxLength: [8, "Description must be atleast 8 characters"],
+    minLength: [200, "Description should be less than 200 characters"],
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: [true, "Category is required"],
+  },
+  thumbnail: {
+    public_id: {
+      type: "String",
+      required: true,
+    },
+    secure_url: {
+      type: "String",
+      required: true,
+    },
+  },
+  lectures: [
+    {
+      title: String,
+      description: String,
+      lectures: {
+        public_id: {
+          type: "String",
+          required: true,
+        },
+        secure_url: {
+          type: "String",
+          required: true,
+        },
+      },
+    },
+  ],
+  numberOfLectures: {
+    type: Number,
+    default: 0  // if there is no lecture present then default value will be 0
+  },
+  createdBy: {
+    type: String,
+    required: [true, 'name of author is required']
+  }
+});
