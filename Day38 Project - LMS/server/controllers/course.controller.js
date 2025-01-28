62b8aef7486216383de0c16cf0ed5f217e38c06b
Indu Rani <raniindu22@gmail.com>
2025-01-28 15:38:08 +0530

    controller function to access course and lecture details

--- Day38 Project - LMS/server/controllers/course.controller.js
+++ Day38 Project - LMS/server/controllers/course.controller.js
@@ -5,27 +5,29 @@ const getAllCourses = async function (req, res, next) {
     // courses ki high level details cahiye, uske andar lectures nahi cahiye
   } catch (error) {
     return next(new AppError(error || "course details not found", 500));
-}
+  }
 };
 
-
-const getLecturesByCourseId = async function () {
-    try {
-        const { id } = req.params;
-        // we can get course id from routes - /:id - through params 
-        const course = await Course.findById(id);
-        // what is course id is my id 
-
-        res.status(200).json({
-            success: true,
-            message: 'course lectures fetched successfully',
-            lectures: course.lectures 
-            // I  don't want to return course details , I just want to return lectures inside that 
-        })
-        
-    } catch (error) {
-        return next(new AppError(error.message, 500));    
+const getLecturesByCourseId = async function (req, res, next) {
+  try {
+    const { id } = req.params;
+    // we can get course id from routes - /:id - through params
+    console.log("Course Id :", id);
+    const course = await Course.findById(id);
+    // what is course id is my id
+    console.log("Course Details :", course);
+    if (!course) {
+      return next(new AppError("Invalid course Id", 400));
     }
+    res.status(200).json({
+      success: true,
+      message: "course lectures fetched successfully",
+      lectures: course.lectures,
+      // I  don't want to return course details , I just want to return lectures inside that
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
 };
 
 export { getAllCourses, getLecturesByCourseId };
