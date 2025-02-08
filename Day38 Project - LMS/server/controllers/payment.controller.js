ebe8067825d62ae6c9b65e0da4eaeb9695138564
Indu Rani <raniindu22@gmail.com>
2025-02-08 20:22:13 +0530

    defining routes for payment method

---
+++ Day38 Project - LMS/server/controllers/payment.controller.js
@@ -0,0 +1,14 @@
+export const getRazorpayApiKey = async(req, res, next) => {};
+
+
+export const buySubscription = async(req, res, next) => {};
+
+
+export const verifySubscription = async(req, res, next) => {};
+
+
+export const cancelSubscription = async(req, res, next) => {};
+
+
+export const allPayment = async(req, res, next) => {};
+
