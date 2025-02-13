91ff1a86ca6da6768fc472af3fc75152e75b2911
Indu Rani <raniindu22@gmail.com>
2025-02-13 21:09:30 +0530

    subscription created successfully

--- Day38 Project - LMS/server/controllers/payment.controller.js
+++ Day38 Project - LMS/server/controllers/payment.controller.js
@@ -37,13 +37,44 @@ export const buySubscription = async(req, res, next) => {
 
     // agar user Admin nahi hai - rezorpay me jana hai subscription create karna hai  - kyuki tabhi to hum payment gateway pe redirect karenge 
 
-    const subscription = await razorpay
     // yaha hume cahiye Razorpay jo server me banaya hai 
+    const subscription = await razorpay.subscriptions.create({
+        plan_id: process.env.RAZORPAY_PLAN_ID,
+        // ish plan id ke corresponding hum har baar  jab load karenge ek naya url generate karte hai , ek naya subscription id generate hota hai jiske corresponding hamara payment hota hai   
+        customer_notify: 1
+        // jaise hi subscription bane customer ke paas notification chala jaye ki subscription ban gaya hai agar payment karna hai to kar lo 
+    })
+
+    // ab subscription ban gaya hai to ish information ko store kar lete hai 
+    //  ishe user level par store karenge
+    user.subscription.id = subscription.id;
+    user.subscription.status = subscription.status;
+
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        message: 'subscribed successfully',
+        subscription_id: subscription.id
+        // hume cahiye subscription id jo client side pe sdk ko cahiye , jaha se wo payment ke liye redirect karega  
+    })
+    // ye subscription ki unique id generate kar dega 
 
 };
+// jaise hi upi  pin dalenge status Active ho jayega 
+// active hone ke baad hum verify karte hai 
 
+// to check ki payment hua ya nahi 
 
-export const verifySubscription = async(req, res, next) => {};
+//  verification karte samay hume client se jo data milne wala hai wo hai - 
+// razorpay_subscription.id, 
+// razorpay_payment.id , 
+// razorpay_signature
+
+export const verifySubscription = async(req, res, next) => {
+    const { id } = req.user;
+    const { razorpay_payment_id, razorpay_signature, razorpay_subscription_id } = req.body;
+};
 
 
 export const cancelSubscription = async(req, res, next) => {};
--- Day38 Project - LMS/server/server.js
+++ Day38 Project - LMS/server/server.js
@@ -12,10 +12,10 @@ cloudinary.v2.config({
 })
 
 
-export const razorpay = new Razorpay(
+export const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET,
-)
+})
 
 app.listen(port, async () => {
   await connectToDb
