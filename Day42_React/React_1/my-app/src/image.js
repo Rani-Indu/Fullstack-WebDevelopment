a75e5aab3849d667dc8a188e7c595a6d8d39fa06
Indu Rani <raniindu22@gmail.com>
2025-05-14 16:15:57 +0530

    js expression , accessing multiple images at a time

--- Day42_React/React_1/my-app/src/image.js
+++ Day42_React/React_1/my-app/src/image.js
@@ -1,11 +1,14 @@
-function Image() {
+function Image(props) {
   return (
     <div>
       <img src="https://images.pexels.com/photos/2664417/pexels-photo-2664417.jpeg?auto=compress&cs=tinysrgb&w=400"
-        alt=""/>
+      alt=""/>
+      {/* <img src={props.src} alt="" /> */}
+      <img src={props.src} alt="" />
     </div>
+
+  // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-YSBJ_14hDiR-QMbYE0Ahu6NfxNpNxGMfA&s
   )
 }
 
 export default Image;
-
